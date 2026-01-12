c992330407f350e621908aa6015ef6bb51ae5b6a
foisaluddin400 <foisalrk2@gmail.com>
2026-01-12 16:43:33 +0600

    asdf

--- src/app/layout/ClientLayout.js
+++ src/app/layout/ClientLayout.js
@@ -78,7 +78,7 @@ export default function ClientLayout({ children }) {
       <div className="flex flex-col bg-[#F7F7F7] min-h-screen max-w-3xl m-auto">
         <div className="fixed z-10 px-3 bg-[#F7F7F7] top-0 w-[48rem]">
           {!hideNavbarNav && (
-            <div className="py-4 pt-8 ">
+            <div className="py-4 pt-4 ">
               <TopBar></TopBar>
             </div>
           )}
